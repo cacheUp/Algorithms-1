651997a865502ef0b89d7ab44698ef8715ab970b
cacheUp <bradb26@gmail.com>
2020-02-20 16:40:45 +0300

    stock prices WOOOHOOO

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -2,14 +2,26 @@
 
 import argparse
 
+
 def find_max_profit(prices):
-  pass
+    cache = []
+
+    def difference(a, listy):
+        if len(listy) > 0:
+            num = a - min(listy)
+            cache.append(num)
+    for i, e in reversed(list(enumerate(prices))):
+        difference(e, prices[:i])
+    return max(cache)
 
 
 if __name__ == '__main__':
-  # This is just some code to accept inputs from the command line
-  parser = argparse.ArgumentParser(description='Find max profit from prices.')
-  parser.add_argument('integers', metavar='N', type=int, nargs='+', help='an integer price')
-  args = parser.parse_args()
+        # This is just some code to accept inputs from the command line
+    parser = argparse.ArgumentParser(
+        description='Find max profit from prices.')
+    parser.add_argument('integers', metavar='N', type=int,
+                        nargs='+', help='an integer price')
+    args = parser.parse_args()
 
-  print("A profit of ${profit} can be made from the stock prices {prices}.".format(profit=find_max_profit(args.integers), prices=args.integers))
+    print("A profit of ${profit} can be made from the stock prices {prices}.".format(
+        profit=find_max_profit(args.integers), prices=args.integers))
